d51fbbc454cfe1080880cef4f2b24b8263343f97
Daniel McGraw <dan.j.mcgraw@gmail.com>
2014-03-11 22:26:12 -0600

    Push it, push it real good.

--- git-literate.pre-push.sh
+++ git-literate.pre-push.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 #
 # git-literate
 # http://danielmcgraw.github.io/git-literate/
 #
-# Protect yourself from thoughtlessly pushing to remote repositories.
+# Protect yourself from accidentally pushing to remote repositories.
 
 randnum=`perl -e "print int rand(99999)"`
 word=`sed $randnum"q;d" /usr/share/dict/words`
