2030a1976f48240d66629b51a262239c85a22c18
Daniel McGraw <dan.j.mcgraw@gmail.com>
2014-03-11 22:23:09 -0600

    Add the specific url modification script.

---
+++ modifications/specific-url.pre-push.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# git-literate
+# http://danielmcgraw.github.io/git-literate/
+#
+# Protect yourself from thoughtlessly pushing to remote repositories.
+#
+# Specific URL modification x`
+
+randnum=`perl -e "print int rand(99999)"`
+word=`sed $randnum"q;d" /usr/share/dict/words`
+
+if [[ "$2" == "git@github.com:danielmcgraw/git-literate.git" ]] 
+then
+  read -p "To push to $1 type '$word': " < /dev/tty
+  exit `[[ "$word" == "$REPLY" ]]`
+fi
